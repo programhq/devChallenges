30ca188724dba585d81a350584b1981d97f69e54
Pascal M <mail@mkpascal.net>
2023-01-22 20:21:01 -0800

    simple challenge I guess?

---
+++ 05/script.js
@@ -0,0 +1,7 @@
+// You can use any Javascript code playground like https://jsfiddle.net/ to solve this.
+
+// You have the following hash:
+
+let pokemonList = {25: "pikachu", 151: "mew", 6: "charizard", 11: "metapod", 54: "psyduck"};
+
+// Sort by ID and print each Pokémon line by line in the console.
